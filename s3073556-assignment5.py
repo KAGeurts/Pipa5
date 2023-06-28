e0463e99ef2466a886ceeee87928252c58960920
KAGeurts <137941186+KAGeurts@users.noreply.github.com>
2023-06-28 11:04:44 +0200

    Update comments

--- s3073556-assignment5.py
+++ s3073556-assignment5.py
@@ -1,36 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-#Making a function to add insulin
-def add_insulin(dose, start_ins_conc = 0, start_glu_conc = 1.2):
-	dt = 1 #timestep in minutes
-	stop_time = 240  #Stop time in minutes
-	
-	depot = dose #unabsorbed insulin in g/l, starting value is dose
-	c_ins = start_ins_conc #insulin concentration in blood in g/l
-	c_glu = start_glu_conc #glucose concentration in blood in g/l
-
-	ka = 0.04 #absorption rate constant in /min
-	ke = 0.15 #elimination rate constant in /min
-	keff = 0.15 #effect rate constant
-	ins_data = []
-	glu_data = []
-
-	#Pharmacokinetic equation
-	for t in np.arange(0, stop_time, dt):
-		d_depot = (-ka*depot)*dt
-		d_ins = (ka*depot - ke*c_ins)*dt
-		depot += d_depot
-		c_ins += d_ins
-		ins_data += [c_ins]
-
-	#Pharmacodynamic equation
-	for c in ins_data:
-		c_glu += -keff*c
-		glu_data += [c_glu]
-
-	return ins_data, glu_data
-
 #Adding meals at random intervals
 def meal(sugar, start_glu_conc = 1.2):
 	dt = 1 #timestep in minutes
@@ -47,3 +17,33 @@ def meal(sugar, start_glu_conc = 1.2):
 			glu_data += [c_glu]
 
 	return glu_data
+
+#This function simulates the glucose decrease from insulin
+def add_insulin(dose, start_ins_conc = 0, start_glu_conc = 1.2):
+	dt = 1 #timestep in minutes
+	stop_time = 240  #Stop time in minutes
+	
+	depot = dose #unabsorbed insulin in g/l, starting value is dose
+	c_ins = start_ins_conc #insulin concentration in blood in g/l
+	c_glu = start_glu_conc #glucose concentration in blood in g/l
+
+	ka = 0.04 #absorption rate constant in /min
+	ke = 0.15 #elimination rate constant in /min
+	keff = 0.15 #effect rate constant
+	ins_data = []
+	glu_data = []
+
+	#This pharmacokinetic equation simulates insulin concentrations
+	for t in np.arange(0, stop_time, dt):
+		d_depot = (-ka*depot)*dt
+		d_ins = (ka*depot - ke*c_ins)*dt
+		depot += d_depot
+		c_ins += d_ins
+		ins_data += [c_ins]
+
+	#This pharmacodynamic equation simulates the effect of insulin on glucose concentrations.
+	for c in ins_data:
+		c_glu += -keff*c
+		glu_data += [c_glu]
+
+	return ins_data, glu_data
